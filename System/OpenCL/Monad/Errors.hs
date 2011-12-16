9a937f4f9f8d95490a5d72f09e5b78ee0ee24fe3
Alex Mason <axman6@gmail.com>
2011-12-17 09:51:45 +1100

    Wrapper for Errors

---
+++ System/OpenCL/Monad/Errors.hs
@@ -0,0 +1,7 @@
+{-| A collection of all the error codes that OpenCL functions return -}
+module System.OpenCL.Monad.Errors (
+	module System.OpenCL.Wrappers.Errors
+	)
+where
+
+import System.OpenCL.Wrappers.Errors

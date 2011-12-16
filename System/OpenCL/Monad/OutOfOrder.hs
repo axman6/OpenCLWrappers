780cbbb451ccf2915ffd24ef02a0dc1385caa85e
Alex Mason <axman6@gmail.com>
2011-12-17 10:51:05 +1100

    Wrapper for OutOfOrder

---
+++ System/OpenCL/Monad/OutOfOrder.hs
@@ -0,0 +1,18 @@
+module System.OpenCL.Monad.OutOfOrder
+    (clEnqueueMarker
+    ,clEnqueueWaitForEvents
+    ,clEnqueueBarrier)
+where 
+
+import qualified System.OpenCL.Wrappers.Helpers as O
+import System.OpenCL.Wrappers.Types
+import System.OpenCL.Monad.Types
+
+clEnqueueMarker :: CommandQueue -> OpenCL Event
+clEnqueueMarker queue = ErrorT $ O.clEnqueueMarker queue
+    
+clEnqueueWaitForEvents :: CommandQueue -> [Event] -> OpenCL ()
+clEnqueueWaitForEvents queue events = ErrorT $ O.clEnqueueWaitForEvents queue events
+
+clEnqueueBarrier :: CommandQueue -> OpenCL () 
+clEnqueueBarrier queue = ErrorT $ O.clEnqueueBarrier queue

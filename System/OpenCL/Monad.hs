ffe5103d00607caa91134ab0227155ec43a1920d
Alex Mason <axman6@gmail.com>
2011-12-17 11:08:56 +1100

    Added Monad module, which exports all the OpenCL monad stuff

--- System/OpenCL/Monad.hs
+++ System/OpenCL/Monad.hs
@@ -1,5 +1,34 @@
+module System.OpenCL.Monad
+    (module System.OpenCL.Monad.CommandQueue
+    ,module System.OpenCL.Monad.Context
+    ,module System.OpenCL.Monad.DeviceInfo
+    ,module System.OpenCL.Monad.Errors
+    ,module System.OpenCL.Monad.Etc
+    ,module System.OpenCL.Monad.EventObject
+    ,module System.OpenCL.Monad.FlushFinish
+    ,module System.OpenCL.Monad.Kernel
+    ,module System.OpenCL.Monad.MemoryObject
+    ,module System.OpenCL.Monad.OutOfOrder
+    ,module System.OpenCL.Monad.PlatformInfo
+    ,module System.OpenCL.Monad.ProgramObject
+    ,module System.OpenCL.Monad.Sampler
+    ,module System.OpenCL.Monad.Types
+    ,module System.OpenCL.Monad.Helpers
+    ) 
+where
 
-import Control.Monad.Error
-
-type OpenCL a = ErrorT ErrorCode IO a
-
+import System.OpenCL.Monad.CommandQueue
+import System.OpenCL.Monad.Context
+import System.OpenCL.Monad.DeviceInfo
+import System.OpenCL.Monad.Errors
+import System.OpenCL.Monad.Etc
+import System.OpenCL.Monad.EventObject
+import System.OpenCL.Monad.FlushFinish
+import System.OpenCL.Monad.Kernel
+import System.OpenCL.Monad.MemoryObject
+import System.OpenCL.Monad.OutOfOrder
+import System.OpenCL.Monad.PlatformInfo
+import System.OpenCL.Monad.ProgramObject
+import System.OpenCL.Monad.Sampler
+import System.OpenCL.Monad.Types
+import System.OpenCL.Monad.Helpers

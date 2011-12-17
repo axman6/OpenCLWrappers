5c1bb235c6b6bec5595027598a919441221c395e
Alex Mason <axman6@gmail.com>
2011-12-17 15:54:42 +1100

    Added lift to export list of Monad.Types

--- System/OpenCL/Monad/Types.hs
+++ System/OpenCL/Monad/Types.hs
@@ -1,6 +1,6 @@
-module System.OpenCL.Monad.Types (OpenCL, ErrorT(..), liftIO) where
+module System.OpenCL.Monad.Types (OpenCL, ErrorT(..), liftIO, lift, runOpenCL) where
 
-import Control.Monad.Error (ErrorT(..), runErrorT, liftIO)
+import Control.Monad.Error (ErrorT(..), runErrorT, liftIO, lift)
 import System.OpenCL.Wrappers.Types (ErrorCode)
 
 type OpenCL a = ErrorT ErrorCode IO a
